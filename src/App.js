b8bcc8938d14f6d3216fa693474f168a5307d457
shraddha-1402 <shraddha.gupta.1402@gmail.com>
2021-07-10 11:22:23 +0530

    completed js of the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+
+const emojiDictionary = {
+  "😃": "Happy",
+  "😔": "Sad",
+  "😂": "Laughing",
+  "😭": "Crying",
+  "😷": "Sick",
+  "😍": "Hearty Eyes",
+};
 
 function App() {
+
+  const [meaning, setMeaning] = useState("");
+
+  const inputChanged = (event) => {
+    let value = emojiDictionary[event.target.value] ? emojiDictionary[event.target.value] : "We do not have the meaning of this emoji :(";
+    setMeaning(value);
+  }
+
+  const emojiClickHandler = (event) => setMeaning(emojiDictionary[event.target.innerText]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Emoji Interpreter</h1>
+      <p>Know your emotions</p>
+      <input onChange={inputChanged} />
+      <p>Meaning: {meaning}</p>
+      <ul>
+        {
+          Object.keys(emojiDictionary).map((item, index) => {
+            return <li
+              key={index}
+              style={{ cursor: 'pointer' }}
+              onClick={emojiClickHandler}
+            >{item}
+            </li>
+          })
+        }
+      </ul>
     </div>
   );
 }
